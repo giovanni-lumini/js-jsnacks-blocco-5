f7aa5857aa3c0f5acdc3bd10b3aef1da8e73f951
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-16 15:28:46 +0200

    snack3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+numbers_plus_one = [];
+
+numbers.forEach(function(element){
+    let n_plus_one = ++element;
+    numbers_plus_one.push(n_plus_one);
+});
+console.log(numbers_plus_one);
