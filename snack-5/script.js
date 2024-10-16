eeccb7e91023f6d3f942b9d3c2e3ae6a01c1ffa2
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-16 15:33:56 +0200

    snack5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,11 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+nums_pari = nums.filter(function(element){
+    if (element % 2 === 0){
+        return true
+    }
+    return false
+});
+console.log(nums_pari);
